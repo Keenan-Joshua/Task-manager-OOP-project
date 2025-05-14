3f238f5c5796c4f41b1fcd6ba61190ddae24b70b
Jojo823-svg <joy.mutinda@strathmore.edu>
2025-05-14 22:14:27 +0300

    made a user class

---
+++ User/User.java
@@ -0,0 +1,24 @@
+package User;
+
+public class User {
+    private String username;
+    private String hashedPassword;
+    private String email;
+
+    public User(String uname, String hashPass, String mail){
+        username = uname;
+        hashedPassword=hashPass;
+        email=mail;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public String getEmail(){
+        return email;
+    }
+    public boolean checkPassword(String pass){
+        return pass.equals(hashedPassword);
+    }
+
+
+}
